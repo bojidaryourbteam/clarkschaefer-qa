0ddba9de16b3ac614fc5a6c8ecc4e16088bd2fe4
bojidaryourbteam <135518370+bojidaryourbteam@users.noreply.github.com>
2023-08-23 17:42:08 +0300

    Update LightBackGroundTheme.js

--- src/components/Card/Themes/LightBackGroundTheme/LightBackGroundTheme.js
+++ src/components/Card/Themes/LightBackGroundTheme/LightBackGroundTheme.js
@@ -23,7 +23,7 @@ const LightBackGroundTheme = (props) => {
       </div>
       <h1>{heading}</h1>
       <p>{subheading}</p>
-      {url && <Link href={url}>Explore</Link>}
+      {url && <Link href={image.url}>Explore</Link>}
     </div>
   );
 };
